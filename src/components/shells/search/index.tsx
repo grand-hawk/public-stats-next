07a102c13d6770ad1b67c531ca18560493946034
astrid <git@astrid.email>
2025-01-27 21:14:59 +0100

    resimplify search input

--- src/components/shells/search/index.tsx
+++ src/components/shells/search/index.tsx
@@ -25,8 +25,8 @@ export default function Search({
     >
       <SearchInput
         disabled={!placeId}
-        setValue={(value) => setQuery(value)}
         value={query}
+        onChange={(event) => setQuery(event.target.value)}
       />
 
       <Box
