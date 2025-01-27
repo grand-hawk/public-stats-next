0b027e885dc9494ffb50a49244a9ee4c8ee67a5c
astrid <git@astrid.email>
2025-01-27 22:06:17 +0100

    dev build if dev env

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 
   output: process.env.NEXT_OUTPUT as NextConfig['output'],
+
+  experimental: {
+    ...(process.env.NODE_ENV === 'development' && {
+      allowDevelopmentBuild: true,
+    }),
+  },
 };
 
 export default nextConfig;
