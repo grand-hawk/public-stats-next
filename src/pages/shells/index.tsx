9ca15399ddc8e98c8472e8ce244a7fa5eb64978e
astrid <git@astrid.email>
2025-01-27 22:51:41 +0100

    testing

--- src/pages/shells/index.tsx
+++ src/pages/shells/index.tsx
@@ -5,25 +5,26 @@ import Search from '@/components/shells/search';
 
 import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
-export const getServerSideProps = (async ({ query: queryParams }) => {
-  return {
-    props: {
-      initialQuery:
-        typeof queryParams.query === 'string' ? queryParams.query : null,
-    },
-  };
-}) satisfies GetServerSideProps<{ initialQuery: string | null }>;
+// export const getServerSideProps = (async ({ query: queryParams }) => {
+//   return {
+//     props: {
+//       initialQuery:
+//         typeof queryParams.query === 'string' ? queryParams.query : null,
+//     },
+//   };
+// }) satisfies GetServerSideProps<{ initialQuery: string | null }>;
 
-export default function WinrateTab({
-  initialQuery,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+// export default function WinrateTab({
+//   initialQuery,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function WinrateTab() {
   return (
     <>
       <Head>
         <title>Search - MTC Stats</title>
       </Head>
 
-      <Search initialQuery={initialQuery} />
+      <Search initialQuery={''} />
     </>
   );
 }
