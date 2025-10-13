da3902f62a166619388a447177ab54e864576f9e
astrid <git@astrid.email>
2025-10-13 23:59:27 +0200

    Remove unused stuff in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,15 @@
 import { Box } from '@chakra-ui/react';
-// import { useIsClient } from '@uidotdev/usehooks';
 import React, { Suspense } from 'react';
 
 import CenterSpinner from '@/components/centerSpinner';
-import Head from '@/components/utils/head';
 import { Provider } from '@/components/ui/provider';
+import Head from '@/components/utils/head';
 import Umami from '@/components/utils/umami';
-// import { usePersistStoreIsHydrated } from '@/hooks/usePersistStoreIsHydrated';
-// import { useVehicleSearchStore } from '@/stores/vehicles/search';
 import { trpc } from '@/utils/trpc';
 
 import type { AppProps } from 'next/app';
 
 export function App({ Component, pageProps }: AppProps) {
-  // const isClient = useIsClient();
-  // const persistedStores = [useVehicleSearchStore];
-  // const persistedStoresAreHydrated = persistedStores.every(
-  //   usePersistStoreIsHydrated,
-  // );
-
   return (
     <>
       <Head />
