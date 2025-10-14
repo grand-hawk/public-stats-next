80968038ee147db5def69617aed44a1f25fb473b
astrid <git@astrid.email>
2025-10-14 17:15:06 +0200

    Image static height

--- src/components/vehicles/vehicle/header.tsx
+++ src/components/vehicles/vehicle/header.tsx
@@ -41,33 +41,29 @@ export default function VehicleHeader({ vehicle }: { vehicle: NamedVehicle }) {
         base: 0,
         md: '1px',
       }}
-      display="grid"
       divideY="1px"
-      gridTemplateRows="2fr 1fr"
-      height="sm"
       width="100%"
     >
-      <Box backgroundColor="bg.panel" position="relative">
+      <Box backgroundColor="bg.panel" height="3xs" position="relative">
         {imageErrored ? (
           <Center height="100%">
             <Text fontWeight="medium">Resource not found</Text>
           </Center>
         ) : (
-          <NextImage
-            key={vehicle.info.slug}
-            alt={`Image of the "${vehicle.info.name}" in Multicrew Tank Combat on Roblox`}
-            blurDataURL="data:image/webp;base64,UklGRooAAABXRUJQVlA4WAoAAAAAAAAAHwAAHwAAVlA4IGwAAACwBACdASogACAAPok2lUglIyIhN+gAoBEJZwDIXHmzSajWQrznMxbR+dwOHsqOAPAZsP004crt8WSSX8AoxpEFm2bGOnGFvmyW0fypFOzSYuYnEYiece44qIIOawb6sV0s9LBRAZlOhQUJwAA="
-            fill
-            placeholder="blur"
-            priority
-            sizes="(min-width: 80rem) 1000px, (min-width: 60rem) 800px, 600px"
-            src={`/assets/vehicles/${vehicle.info.slug}.png`}
-            style={{
-              objectFit: 'cover',
-            }}
-            onError={() => setImageErrored(true)}
-            onLoad={() => setImageErrored(false)}
-          />
+          <Box asChild objectFit="cover">
+            <NextImage
+              key={vehicle.info.slug}
+              alt={`Image of the "${vehicle.info.name}" in Multicrew Tank Combat on Roblox`}
+              blurDataURL="data:image/webp;base64,UklGRooAAABXRUJQVlA4WAoAAAAAAAAAHwAAHwAAVlA4IGwAAACwBACdASogACAAPok2lUglIyIhN+gAoBEJZwDIXHmzSajWQrznMxbR+dwOHsqOAPAZsP004crt8WSSX8AoxpEFm2bGOnGFvmyW0fypFOzSYuYnEYiece44qIIOawb6sV0s9LBRAZlOhQUJwAA="
+              fill
+              placeholder="blur"
+              priority
+              sizes="(min-width: 80rem) 1000px, (min-width: 60rem) 800px, 600px"
+              src={`/assets/vehicles/${vehicle.info.slug}.png`}
+              onError={() => setImageErrored(true)}
+              onLoad={() => setImageErrored(false)}
+            />
+          </Box>
         )}
 
         {Object.keys(vehicle.info.externalLinks).length > 0 && (
