eeb9da8a317e915c2aa98621ef6560a0065cf9b3
astrid <git@astrid.email>
2025-11-02 22:33:11 +0100

    Code recipe

---
+++ src/components/utils/chakra/recipes/code.ts
@@ -0,0 +1,7 @@
+import { defineRecipe } from '@chakra-ui/react';
+
+export const codeRecipe = defineRecipe({
+  base: {
+    borderRadius: 'none',
+  },
+});
--- src/components/utils/chakra/recipes/index.ts
+++ src/components/utils/chakra/recipes/index.ts
@@ -2,9 +2,11 @@ import { buttonRecipe } from './button';
 import { segmentGroupRecipe } from './segmentGroup';
 import { selectRecipe } from './select';
 import { alertRecipe } from '@/components/utils/chakra/recipes/alert';
+import { codeRecipe } from '@/components/utils/chakra/recipes/code';
 
 export const recipes = {
   button: buttonRecipe,
+  code: codeRecipe,
 };
 
 export const slotRecipes = {
