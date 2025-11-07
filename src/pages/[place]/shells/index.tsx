5a7198f50da0709272bb6c517be46893816cd8d1
astrid <git@astrid.email>
2025-11-07 15:50:10 +0100

    Title head to vehicles/index and shells/index

--- src/pages/[place]/shells/index.tsx
+++ src/pages/[place]/shells/index.tsx
@@ -1,4 +1,5 @@
 import { Flex } from '@chakra-ui/react';
+import Head from 'next/head';
 import React from 'react';
 import { GrDocumentMissing } from 'react-icons/gr';
 
@@ -6,15 +7,28 @@ import SearchLayout from '@/components/searchLayout/layout';
 import ShellsSearchSidebar from '@/components/shells/searchSidebar';
 import { EmptyState } from '@/components/ui/empty-state';
 import Layout from '@/components/utils/layout';
+import { usePlaceInitials } from '@/hooks/usePlaceInitials';
+import { formatTitle } from '@/utils/formatTitle';
 
 export default function PlaceShells() {
+  const initials = usePlaceInitials()!;
+
   return (
-    <Layout noPadding>
-      <SearchLayout sidebar={<ShellsSearchSidebar />}>
-        <Flex alignItems="center" height="100%">
-          <EmptyState icon={<GrDocumentMissing />} title="No shell selected" />
-        </Flex>
-      </SearchLayout>
-    </Layout>
+    <>
+      <Head>
+        <title>{formatTitle('Shells', initials)}</title>
+      </Head>
+
+      <Layout noPadding>
+        <SearchLayout sidebar={<ShellsSearchSidebar />}>
+          <Flex alignItems="center" height="100%">
+            <EmptyState
+              icon={<GrDocumentMissing />}
+              title="No shell selected"
+            />
+          </Flex>
+        </SearchLayout>
+      </Layout>
+    </>
   );
 }
--- src/pages/[place]/vehicles/index.tsx
+++ src/pages/[place]/vehicles/index.tsx
@@ -1,4 +1,5 @@
 import { Flex } from '@chakra-ui/react';
+import Head from 'next/head';
 import React from 'react';
 import { GrDocumentMissing } from 'react-icons/gr';
 
@@ -6,18 +7,28 @@ import SearchLayout from '@/components/searchLayout/layout';
 import { EmptyState } from '@/components/ui/empty-state';
 import Layout from '@/components/utils/layout';
 import VehiclesSearchSidebar from '@/components/vehicles/searchSidebar';
+import { usePlaceInitials } from '@/hooks/usePlaceInitials';
+import { formatTitle } from '@/utils/formatTitle';
 
 export default function PlaceVehicles() {
+  const initials = usePlaceInitials()!;
+
   return (
-    <Layout noPadding>
-      <SearchLayout sidebar={<VehiclesSearchSidebar />}>
-        <Flex alignItems="center" height="100%">
-          <EmptyState
-            icon={<GrDocumentMissing />}
-            title="No vehicle selected"
-          />
-        </Flex>
-      </SearchLayout>
-    </Layout>
+    <>
+      <Head>
+        <title>{formatTitle('Vehicles', initials)}</title>
+      </Head>
+
+      <Layout noPadding>
+        <SearchLayout sidebar={<VehiclesSearchSidebar />}>
+          <Flex alignItems="center" height="100%">
+            <EmptyState
+              icon={<GrDocumentMissing />}
+              title="No vehicle selected"
+            />
+          </Flex>
+        </SearchLayout>
+      </Layout>
+    </>
   );
 }
