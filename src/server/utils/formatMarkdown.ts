f0434111cbb1a8f87ae75d98c4c4a1d554facc21
astrid <git@astrid.email>
2025-12-18 15:23:01 +0100

    Include base url in note

--- src/server/utils/formatMarkdown.ts
+++ src/server/utils/formatMarkdown.ts
@@ -1,5 +1,7 @@
 import prettier from 'prettier';
 
+import { getBaseUrl } from '@/utils/trpc';
+
 export function escapeMarkdownLink(link: string) {
   return link.replace(/[[\]()]/g, '\\$&');
 }
@@ -8,6 +10,7 @@ export async function formatMarkdown(markdown: string) {
   markdown = `
 > [!NOTE]  
 > Remove \`/md/\` and \`.md\` from the URL to link to the regular page.
+> Base URL: \`${getBaseUrl()}\`
 
 ${markdown}
 `;
