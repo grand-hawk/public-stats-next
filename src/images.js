e9da3c7c0861b1d246aeea174fb0620981624f96
astrid <git@astrid.email>
2026-01-26 22:49:58 +0100

    Fix image loader having a dependency

--- src/images.js
+++ src/images.js
@@ -1,6 +1,4 @@
-import { env } from '@/env';
-
 export default function imageLoader({ quality, src, width }) {
   const path = new URL(src).pathname;
-  return `${env.NEXT_PUBLIC_IMAGE_LOADER}${path}?w=${width}&q=${quality ?? 75}`;
+  return `${process.env.NEXT_PUBLIC_IMAGE_LOADER}${path}?w=${width}&q=${quality ?? 75}`;
 }
