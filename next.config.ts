3018dcf450162cbf967287c6967d5cca3c67359c
astrid <git@astrid.email>
2026-02-10 22:47:12 +0100

    Enable React compiler

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  reactCompiler: true,
 
   output: process.env.NEXT_OUTPUT as NextConfig['output'],
 
